7211e014a2fcee61424e51b23500e5d9e9555daa
Yuri Cunha <cunha.ysc@gmail.com>
2023-12-15 06:54:36 -0300

    building ShoppingFormView

--- MuambasUSA/Views/ShoppingForm/ShoppingFormView.swift
+++ MuambasUSA/Views/ShoppingForm/ShoppingFormView.swift
@@ -6,10 +6,39 @@
 //
 
 import SwiftUI
-
+import PhotosUI
 struct ShoppingFormView: View {
+    @State private var productName = ""
+    @State private var stateTax = 0.0
+    @State private var productPrice = 0.0
+    @State private var isCreditCar = false
+    @State private var prouctImage: PhotosPickerItem? = nil
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section("NOME DO PRODUTO") {
+                TextField("Escreva o nome do produto",text: $productName)
+            }
+            
+            Section("IMPOSTO DO ESTADO") {
+                TextField("Escreva o imposto do estado",value: $stateTax,formatter: NumberFormatter())
+            }
+            
+            Section("VALOR DO PRODUTO") {
+                TextField("Escreva custo do produto",value: $productPrice,formatter: NumberFormatter())
+            }
+            
+            Section("MEIO DE PAGAMENTO") {
+                Toggle("Pagou com cartão?", isOn: $isCreditCar)
+            }
+            
+            Section("FOTO") {
+                PhotosPicker(selection: $prouctImage) {
+                    Label("Escolher foto", systemImage: "giftcard.fill")
+                }
+               
+            }
+        }
+        .navigationTitle("Cadastro de produto")
     }
 }
 
