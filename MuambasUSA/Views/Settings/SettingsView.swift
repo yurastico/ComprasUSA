f7f34a4e6c1ed4c1a0acb6733d46fa63625525c0
Yuri Cunha <cunha.ysc@gmail.com>
2023-12-15 05:41:48 -0300

    building SettingsView

--- MuambasUSA/Views/Settings/SettingsView.swift
+++ MuambasUSA/Views/Settings/SettingsView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @State private var dollar = 0.0
+    @State private var iof = 0.0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section("Cotação do dólar (R$)") {
+                    TextField("R$ 0.0",value: $dollar, formatter: NumberFormatter())
+                }
+                
+                Section("IOF (%)") {
+                    TextField("0.0",value: $iof, formatter: NumberFormatter())
+                }
+            }
+            .navigationTitle("Ajustes")
+        }
     }
 }
 
