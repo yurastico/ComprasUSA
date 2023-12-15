a2a7c2c080256de016b372ca86c8a0e1460a4376
Yuri Cunha <cunha.ysc@gmail.com>
2023-12-15 03:17:05 -0300

    Setting up project

---
+++ MuambasUSA/MuambasUSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuambasUSAApp.swift
+//  MuambasUSA
+//
+//  Created by Yuri Cunha on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MuambasUSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# MuambasUSA
+## MuambasUSA é o melhor jeito para voce ter controle de suas compras nos Estados Unidos
+
+### Tenha ocntrole das suas compras da melhor forma!
+
+* Cotação do dólar em tempo real
+* Cálculo de IOF
+
+
