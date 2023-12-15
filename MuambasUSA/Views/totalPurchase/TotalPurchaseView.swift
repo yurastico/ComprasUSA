90028b1619ac537b93369a1fd094976ce4311cd7
Yuri Cunha <cunha.ysc@gmail.com>
2023-12-15 06:02:07 -0300

    buiding TotalPurchaseView

--- MuambasUSA/Views/totalPurchase/TotalPurchaseView.swift
+++ MuambasUSA/Views/totalPurchase/TotalPurchaseView.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct TotalPurchaseView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 32) {
+                VStack(alignment: .leading) {
+                    Text("Valor dos produtos ($)")
+                    Text("$ 1,168.00")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundStyle(.blue)
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Total com impostos ($)")
+                    
+                    Text("$ 1,168.00")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundStyle(.red)
+                }
+                VStack(alignment: .leading) {
+                    Text("Valor final em reais")
+                    Text("R$ 4.500,00")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundStyle(.green)
+                }
+                
+            }
+            .frame(maxWidth: .infinity,alignment: .leading)
+            .padding()
+            .navigationTitle("Resumo da compra")
+            Spacer()
+        }
     }
 }
 
