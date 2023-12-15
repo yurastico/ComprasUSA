a7171ebf42efad3f83a8b94871b259d77a451c86
Yuri Cunha <cunha.ysc@gmail.com>
2023-12-15 05:21:31 -0300

    building ShoppingListView

--- MuambasUSA/Views/ShoppingList/ShoppingListView.swift
+++ MuambasUSA/Views/ShoppingList/ShoppingListView.swift
@@ -8,10 +8,42 @@
 import SwiftUI
 
 struct ShoppingListView: View {
+    let shoppingItems = [String]()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Group {
+                if shoppingItems.isEmpty {
+                    Text("Sua lista de compras está vazia")
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .italic()
+                        
+                } else {
+                    List {
+                        ForEach(shoppingItems,id: \.self) { item in
+                            Text(item)
+                        }
+                        .onDelete(perform: deleteItem)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Lista de compras")
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
+    }
+    private func deleteItem(at offsets: IndexSet) {
+        print(#function)
     }
 }
+
 
 #Preview {
     ShoppingListView()
