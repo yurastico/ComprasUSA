f0e1a8c095fdd3979c1d4f23e30c3f305f67db4d
yurastico <cunha.ysc@gmail.com>
2024-02-03 15:01:05 -0300

    fixing keyboard locking user on settingsView

--- ComprasUSA/Views/Settings/SettingsView.swift
+++ ComprasUSA/Views/Settings/SettingsView.swift
@@ -15,20 +15,32 @@ enum UserSettings {
 struct SettingsView: View {
     @AppStorage("dollar") private var dollar: Double = 0.049
     @AppStorage("IOF") private var iof: Double = 5.38
+    @FocusState var isInputActive:Bool
     var body: some View {
         NavigationStack {
             Form {
                 Section("Cotação do dólar (R$)") {
-                    TextField("R$ 0.0",value: $dollar, format: .percent)
+                    TextField("R$ 0.0",value: $dollar, format: .currency(code: "BRL"))
                         .keyboardType(.decimalPad)
+                        .focused($isInputActive)
                 }
                 
                 Section("IOF (%)") {
-                    TextField("0.0",value: $iof, format: .number)
+                    TextField("0%",value: $iof, format: .percent)
                         .keyboardType(.decimalPad)
+                        .focused($isInputActive)
                 }
             }
             .navigationTitle("Ajustes")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        isInputActive = false
+                    }
+                }
+            }
         }
     }
 }
